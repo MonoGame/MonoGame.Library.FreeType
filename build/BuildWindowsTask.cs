4df6c4a07c66f6985c7eafc89b38e93b88c4350e
Christopher Whitley <aristurtledev@gmail.com>
2023-11-01 23:56:10 -0400

    Added windows build step

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -1,13 +1,24 @@
 
+using Cake.Common.Tools.MSBuild;
+
 namespace BuildScripts;
 
 [TaskName("Build Windows")]
+[IsDependentOn(typeof(PrepTask))]
 public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 {
     public override bool ShouldRun(BuildContext context) => context.IsRunningOnWindows();
 
     public override void Run(BuildContext context)
     {
-        
+        MSBuildSettings buildSettings = new()
+        {
+            Verbosity = Verbosity.Normal,
+            Configuration = "Release",
+            PlatformTarget = PlatformTarget.x64
+        };
+
+        context.MSBuild("freetype/builds/windows/vc2010/freetype.vcxproj", buildSettings);
+        context.CopyFile("freetype-demos/bin/freetype.dll", $"{context.ArtifactsDir}/freetype.dll");
     }
 }
