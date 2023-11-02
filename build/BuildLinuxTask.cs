33cea8977bf6ba87f277fdde07079b97e39a9d1d
harry.cpp <harry.cpp@outlook.com>
2023-11-02 10:43:59 +0100

    Add base macOS and Linux instructions

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -2,12 +2,32 @@
 namespace BuildScripts;
 
 [TaskName("Build Linux")]
+[IsDependentOn(typeof(PrepTask))]
 public sealed class BuildLinuxTask : FrostingTask<BuildContext>
 {
     public override bool ShouldRun(BuildContext context) => context.IsRunningOnLinux();
 
     public override void Run(BuildContext context)
     {
-        
+        // Make sure it statically links the dpeendencies
+        context.ReplaceTextInFiles("freetype/CMakeLists.txt", "# Find dependencies", "set(CMAKE_FIND_LIBRARY_SUFFIXES \".a\")");
+
+        // Build
+        var buildDir = "freetype/build";
+        context.CreateDirectory(buildDir);
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "../ -DBUILD_SHARED_LIBS=true -DFT_DISABLE_HARFBUZZ=TRUE -DCMAKE_BUILD_TYPE=Release" });
+        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildDir });
+
+        foreach (var filePath in Directory.GetFiles("freetype/build"))
+        {
+            if (!filePath.Contains(".so") ||
+                File.GetAttributes(filePath).HasFlag(FileAttributes.ReparsePoint))
+                continue;
+
+            context.CopyFile(filePath, $"{context.ArtifactsDir}/libfreetype.so");
+            return;
+        }
+
+        throw new Exception("No built library found :(");
     }
 }
--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -1,13 +1,36 @@
 
+using Cake.Cli;
+using Cake.Common.Diagnostics;
+
 namespace BuildScripts;
 
 [TaskName("Build macOS")]
+[IsDependentOn(typeof(PrepTask))]
 public sealed class BuildMacOSTask : FrostingTask<BuildContext>
 {
     public override bool ShouldRun(BuildContext context) => context.IsRunningOnMacOs();
 
     public override void Run(BuildContext context)
     {
-        
+        // Make sure it statically links the dpeendencies
+        context.ReplaceTextInFiles("freetype/CMakeLists.txt", "# Find dependencies", "set(CMAKE_FIND_LIBRARY_SUFFIXES \".a\")");
+
+        // Build
+        var buildDir = "freetype/build";
+        context.CreateDirectory(buildDir);
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "../ -DBUILD_SHARED_LIBS=true -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 -DFT_DISABLE_HARFBUZZ=TRUE -DCMAKE_BUILD_TYPE=Release" });
+        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildDir });
+
+        foreach (var filePath in Directory.GetFiles("freetype/build"))
+        {
+            if (!filePath.EndsWith(".dylib") ||
+                File.GetAttributes(filePath).HasFlag(FileAttributes.ReparsePoint))
+                continue;
+
+            context.CopyFile(filePath, $"{context.ArtifactsDir}/libfreetype.dylib");
+            return;
+        }
+
+        throw new Exception("No built library found :(");
     }
 }
