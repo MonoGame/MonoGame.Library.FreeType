5bba3e01013b19a7fd435a5ad3fef40d0cda1dcf
harry.cpp <harry.cpp@outlook.com>
2023-11-02 01:12:01 +0100

    A new set of base scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# MonoGame.Library.FreeType
+
+This repository contains build scripts for building and deploying FreeType for use with MonoGame.
---
+++ build/BuildWindowsTask.cs
@@ -0,0 +1,13 @@
+
+namespace BuildScripts;
+
+[TaskName("Build Windows")]
+public sealed class BuildWindowsTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) => context.IsRunningOnWindows();
+
+    public override void Run(BuildContext context)
+    {
+        
+    }
+}
