1c4a92bcf02d5a323099166f95bc826ed632e4c0
harry.cpp <harry.cpp@outlook.com>
2023-11-08 10:01:41 +0100

    Use buildscripts submodule

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -1,11 +1,9 @@
 
-using Cake.Cli;
-using Cake.Common.Diagnostics;
-
 namespace BuildScripts;
 
 [TaskName("Build macOS")]
 [IsDependentOn(typeof(PrepTask))]
+[IsDependeeOf(typeof(BuildLibraryTask))]
 public sealed class BuildMacOSTask : FrostingTask<BuildContext>
 {
     public override bool ShouldRun(BuildContext context) => context.IsRunningOnMacOs();
